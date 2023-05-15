cd88e3d405033dd9d8ad0a2dbbc26d3ea332e00b
andrewakligo <jaakligo@gmail.com>
2023-05-15 06:57:55 +0000

    Update

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,10 @@
+/**
+ * Displays a message on the STDOUT.
+ * @param {String} msg The message to display.
+ * @author Andrew Akligo <https://github.com/andrewakligo>
+ */
+const displayMessage = (msg) => {
+  console.log(msg);
+};
+
+module.exports = displayMessage;
